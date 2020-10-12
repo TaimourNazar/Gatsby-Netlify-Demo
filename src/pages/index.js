571b8c757f1a88deca651fbcade8e4b47c7bfc0c
Taimour <taimour.nazar1@gmail.com>
2020-10-12 20:05:40 +0500

    gatsby netlify updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 export default function Home() {
-  return <div>
+  return (<div>
     <h1>Hello world! Bootcamp2020</h1>
     <h2>Gatsby Netlify Deployment Demo</h2>
-  </div>
+  </div>)
 }
